0e2661784a300c4619352db6c48d759c9fe38f1a
omer sahın <omer.fs@hotmail.com>
2021-12-21 16:03:48 +0300

    fifairf

--- sso/client/src/App.js
+++ sso/client/src/App.js
@@ -1,5 +1,8 @@
 import './App.css';
 
+var pbkdf2 = require('pbkdf2')
+var derivedKey = pbkdf2.pbkdf2Sync('password', 'salt', 1, 32, 'sha512')
+
 function App() {
   return (
     <div className="App">
