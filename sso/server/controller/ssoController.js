099a3e9846c3c7ca6611b20ca02ccde17093148e
yrtby <anilcanyurtbay@hotmail.com>
2021-12-26 02:00:52 +0300

    Refactor backend

--- sso/server/controller/ssoController.js
+++ sso/server/controller/ssoController.js
@@ -1,5 +1,7 @@
 const db = require("../model");
 const { v4: uuidv4 } = require("uuid");
+const sha256 = require('js-sha256');
+const bcrypt = require('bcrypt');
 
 const User = db.user;
 const Url = db.url;
@@ -9,21 +11,41 @@ const Token = db.token;
 // Response: If success, (success, user_id, access_token) / else (fail)
 exports.IsAuthorized = async (req, res) => {
   // User params catching from req
-  const username = req.body.username;
-  const password = req.body.password;
-  const reqRedirectUrl = req.body.url;
+  const {redirectionURL} = req.query;
+  const {username, password, pass_hash} = req.body;
+  let getPassword = password;
+  const reqIp = "127.0.0.1";
+  const salt="qwe123asd123zxc";
 
   try {
     let textUrl;
 
-    // Check user params
-    if (!(username && password && reqRedirectUrl)) {
+    if(!redirectionURL){
       return res.status(409).json({
         stat: "fail",
-        message: "All input are required.",
+        message: "Redirect URL is reqiured."
+      })
+    }
+
+    if(pass_hash == null && pass_hash == undefined){
+      return res.status(409).json({
+        stat: "fail",
+        message: "All inputs are required.",
       });
     }
 
+    // Check user params
+    if (!(username && password)) {
+      return res.status(409).json({
+        stat: "fail",
+        message: "All inputs are required.",
+      });
+    }
+
+    if(pass_hash){
+      getPassword = sha256(getPassword+salt);
+    }
+    
     // If there is user params, check user info
     const user = await User.findOne({ where: { username: username } });
 
@@ -34,9 +56,11 @@ exports.IsAuthorized = async (req, res) => {
         message: "User not found.",
       });
     }
+    
+    const decryptedPassword = await bcrypt.compare(getPassword, user.user_password)
 
     // If user found, password not correct
-    if (user.dataValues.user_password != password) {
+    if (!decryptedPassword) {
       return res.status(409).json({
         stat: "fail",
         message: "Password not match.",
@@ -65,32 +89,33 @@ exports.IsAuthorized = async (req, res) => {
       textUrl = mapUrl;
     }
 
-    var regexUrl = new RegExp(reqRedirectUrl, "g");
+    var regexUrl = new RegExp(redirectionURL, "g");
     if (!textUrl.match(regexUrl)) {
       return res.status(409).json({
         stat: "fail",
-        message: "You are not authorized this url.",
+        message: "URL not found.",
       });
     }
 
     // If password correct; generate token guid and TTL
     const access_token = uuidv4();
     const date = new Date(Date.now());
-    date.setMinutes(date.getMinutes() + 1);
+    date.setMinutes(date.getMinutes() + 5);
 
     // Create user token
     await Token.create({
-      ip: "127.0.0.1",
+      ip: reqIp,
       url: textUrl,
       token: access_token,
       ttl: date.toString(),
+      user_id: user.id
     });
 
     // Return success message and token information.
     return res.status(200).json({
       stat: "success",
-      message: "Login successfull",
-      user_id: user.dataValues.id,
+      message: "login success",
+      user_id: user.id,
       access_token: access_token,
     });
   } catch (err) {
@@ -104,10 +129,8 @@ exports.IsAuthorized = async (req, res) => {
 // Response: If success, (success, access_token) / else (fail)
 exports.IsAccessTokenValid = async (req, res) => {
   // Token params catching from req
-  const reqToken = req.body.token || req.headers["authorization"];
+  const reqToken = req.headers["authorization"];
   const reqIP = "127.0.0.1";
-
-  console.log(reqToken);
 
   try {
     // Create now date
@@ -121,9 +144,19 @@ exports.IsAccessTokenValid = async (req, res) => {
       });
     }
 
+    const bearerTag = reqToken.split(" ")[0];
+    const token = reqToken.split(" ")[1];
+  
+    if (bearerTag != "Bearer") {
+      return res.json({
+        stat: "fail",
+        messsage: "Authorization need Bearer Token"
+      });
+    }
+
     // If there is token params, check token info
     const getToken = await Token.findAll({
-      where: { ip: reqIP, token: reqToken },
+      where: { ip: reqIP, token: token },
     });
 
     // If is not found
@@ -142,68 +175,26 @@ exports.IsAccessTokenValid = async (req, res) => {
 
     // If now date bigger than token TTL
     if (date > tokenTTL) {
-      await Token.destroy({ where: { token: reqToken } });
+      //await Token.destroy({ where: { token: token } });
       return res.status(409).json({
         stat: "fail",
         message: "You are not authorized.",
       });
     } else {
-      if (req.body.from == "middleware") {
-        return res.status(200).json({
-          stat: "success",
-        });
-      }
+      const date = new Date(Date.now());
+      date.setMinutes(date.getMinutes() + 30);
+
       // Create new token
-      const access_token = uuidv4();
-      // Update old token
       await Token.update(
-        { token: access_token },
+        { ttl: date.toString() },
         { where: { id: getTokenId } }
       );
+
       // If token update, return success and token
       return res.status(200).json({
-        stat: "success",
-        access_token: access_token,
+        stat: "success"
       });
     }
-  } catch (err) {
-    // Catch error
-    return res.status(500).json({
-      err,
-    });
-  }
-};
-
-// Request: (url)
-// Response: If success, (success, access_token) / else (fail)
-exports.CheckUrl = async (req, res) => {
-  // URL params catching from req
-  const url = req.body.url;
-
-  try {
-    // Check url params
-    if (!url) {
-      return res.status(409).json({
-        stat: "fail",
-        message: "URL input required",
-      });
-    }
-
-    // If there is token params, check url
-    const checkUrl = await Url.findOne({ where: { url: url } });
-
-    // If is not found
-    if (!checkUrl) {
-      return res.status(401).json({
-        stat: "fail",
-        message: "You are not authorized .",
-      });
-    }
-
-    // If url found, return success
-    return res.status(200).json({
-      stat: "success",
-    });
   } catch (err) {
     // Catch error
     return res.status(500).json({
--- user-api/server/controller/appController.js
+++ user-api/server/controller/appController.js
@@ -1,49 +1,156 @@
 const db = require("../model");
+const sha256 = require('js-sha256');
+const bcrypt = require('bcrypt');
+
+const User = db.user;
+
+const handleQuery = [
+  {name:"users_username_unique", value:"Kullanıcı adı sistemde kayıtlı."},
+  {name:"users_user_email_unique", value:"Email sistemde kayıtlı."},
+]
 
 exports.createUser = async (req, res) => {
+  
+  const { username, user_name, user_surname, user_password, user_email, user_type, pass_hash } = req.body;
+  let getPassword = user_password;
+  const salt="qwe123asd123zxc";
+
+  if(pass_hash == null && pass_hash == undefined){
+    return res.status(409).json({
+      stat: "fail",
+      message: "All inputs are required."
+    });
+  }
+
+  if(!(username && user_name && user_surname && user_password && user_email && user_type)){
+    return res.status(409).json({
+      "stat":"fail",
+      "message":"All inputs are required."
+    });
+  }
+
+  if(pass_hash){
+    getPassword = sha256(getPassword+salt);
+  }
+
+  const encryptedPassword = await bcrypt.hash(getPassword,10);
+
   await db.sequelize
     .query(
       "CALL createUser (:username, :user_name, :user_surname, :user_password, :user_email, :user_type)",
       {
         replacements: {
-          username: req.body.username,
-          user_name: req.body.user_name,
-          user_surname: req.body.user_surname,
-          user_password: req.body.user_password,
-          user_email: req.body.user_email,
-          user_type: req.body.user_type,
+          username: username,
+          user_name: user_name,
+          user_surname: user_surname,
+          user_password: encryptedPassword,
+          user_email: user_email,
+          user_type: user_type,
         },
       }
     )
     .then(() => {
-      res.status(201).json({ isSuccess: true });
+      return res.status(201).json({ 
+        "stat": "success",
+        message: {
+          username: username,
+          user_name: user_name,
+          user_surname: user_surname,
+          user_password: encryptedPassword,
+          user_email: user_email,
+          user_type: user_type
+        }
+      });
     })
-    .catch(() => {
-      res.status(400).json({ isSuccess: false });
+    .catch((err) => {
+      let findPath = err.errors.find(o => o.path)
+      let obj = handleQuery.find(o => o.name === findPath.path);
+      return res.status(500).json({
+        stat: "fail" ,
+        message: obj.value
+      });
     });
 };
+
 exports.deleteUser = async (req, res) => {
+
+  const {user_id} = req.params;
+
+  const user = await User.findOne({where: {id:user_id}});
+
+  if(!user){
+    return res.status(409).json({
+      "stat": "fail",
+      "message": "User not found."
+    })
+  }
+
+  if(user_id == user.id){
+    return res.status(409).json({
+      "stat": "fail",
+      "message": "You cannot delete yourself."
+    })
+  }
+
   await db.sequelize
-    .query("CALL `deleteUser` (:user_id)", {
+    .query("CALL deleteUser (:user_id)", {
       replacements: {
-        user_id: req.params.user_id,
+        user_id: user_id,
       },
     })
     .then(() => {
-      res.status(200).json({ isSuccess: true });
+      res.status(200).json({ 
+        "stat": "success" 
+      });
     })
-    .catch(() => {
-      res.status(400).json({ isSuccess: false });
+    .catch((err) => {
+      res.status(500).json({ 
+        "stat": "fail"
+      });
     });
 };
 
 exports.updateUser = async (req, res) => {
+
+  const { username, user_name, user_surname, user_password, user_email, user_type, pass_hash } = req.body;
+  const {user_id} = req.params;
+  let getPassword = user_password;
+  const salt="qwe123asd123zxc";
+
+
+  const user = await User.findOne({where: {id:user_id}});
+
+  if(!user){
+    return res.status(409).json({
+      "stat": "fail",
+      "message": "User not found"
+    })
+  }
+
+  if(pass_hash == null && pass_hash == undefined){
+    return res.status(409).json({
+      stat: "fail",
+      message: "All inputs are required."
+    });
+  }
+
+  if(!(username && user_name && user_surname && user_email && user_type)){
+    return res.status(409).json({
+      "stat":"fail",
+      "message":"All inputs are required."
+    });
+  }
+
+  if(pass_hash){
+    getPassword = sha256(getPassword+salt);
+  }
+
   await db.sequelize
     .query(
-      "CALL `updateUser` (:user_id, :username, :user_name, :user_surname, :user_password, :user_email, :user_type)",
+      "CALL updateUser (:user_id, :username, :user_name, :user_surname, :user_password, :user_email, :user_type)",
       {
         replacements: {
-          user_id: req.params.user_id,
+          user_id: user_id,
           username: req.body.username,
           user_name: req.body.user_name,
           user_surname: req.body.user_surname,
@@ -54,17 +161,59 @@ exports.updateUser = async (req, res) => {
       }
     )
     .then(() => {
-      res.status(201).json({ isSuccess: true });
+      res.status(201).json({
+        "stat": "success",
+        message: {
+          username: username,
+          user_name: user_name,
+          user_surname: user_surname,
+          user_email: user_email,
+          user_type: user_type
+        }
+      });
     })
     .catch(() => {
-      res.status(400).json({ isSuccess: false });
+      res.status(500).json({ 
+        stat: "fail"
+      });
     });
 };
 
 exports.getListOfUsers = async (req, res) => {
   await db.sequelize.query("CALL getListOfUsers () ").then((data) => {
-    res.status(200).json(data);
+    res.status(200).json({
+      "stat": "success",
+      "message": data
+    });
   }).catch(() => {
-    res.status(400).json({ isSuccess: false });
+    res.status(500).json({ 
+      "stat": "fail"
+    });
   })
 };
+
+exports.getUserInfo = async (req, res) => {
+  try{
+    const {id} = req._user;
+    const user = await User.findOne({where: {id:id}});
+
+    return res.status(200).json({
+      "stat":"success",
+      "data": {
+        id: user.id,
+        username: user.username,
+        user_name: user.user_name,
+        user_surname: user.user_surname,
+        user_email: user.user_email,
+        user_type: user.user_type
+      }
+    })
+  } catch(err){
+    return res.status(500).json({
+      "stat": "fail",
+      "message": err
+    })
+  }
+
+}
+
