e6d1f78d801726446b151be9542944d5b995767b
Ömer Faruk ŞAHİN <79583574+omerfaruk-sahin@users.noreply.github.com>
2021-12-27 17:11:17 +0300

    Update app.test.js

--- sso/server/test/app.test.js
+++ sso/server/test/app.test.js
@@ -1,12 +1,22 @@
 const chai = require("chai");
 const chaiHttp = require("chai-http");
 const expect = chai.expect
-const db = require("../model");
-// const User = db.user;
 chai.use(chaiHttp);
 const server = require("../app")
 
 describe("SSO contoller", async () => {
+    before(function( done ) {
+        server.listen(3100, function(err) {
+            done(err);
+        })
+    });
+    
+    afterAll(function( done ) {
+        server.close(function(err) {
+            done(err);
+        });
+    });
+    
     it("Post isAuthorized", done => {
         chai.request(server).post("/?redirectURL=http://localhost:3020")
             .send({
@@ -16,18 +26,30 @@ describe("SSO contoller", async () => {
             })
             .end((error, res) => {
                 console.log(res.body.stat)
-                expect(res.body.stat).to.be.equal('success')
                 const token = res.body.access_token
                 global.token = token
-                done()
 
+                // try {expect(res.status).to.be.equal(200)}
+                // catch(err) {
+                //     console.log('hata')
+                //     expect(res.status).to.be.equal(404)
+                // }
+
+                expect(res.headers['content-type']).to.equal('application/json; charset=utf-8')
+                expect(res.body.stat).to.be.equal('success')
+                expect(res.body.message).to.be.equal('login success')
+                expect(res.body.access_token).to.be.equal(token)
+
+                done()
             });
     });
     it("Get isAccessTokenValid", done => {
-        chai.request(server).get("/validate")
-            .set("Authorization", "Bearer " + token)
-            .end((error, res) => {
-                console.log(res.body.stat)
+        chai.request(server).get("/avalidate")
+            .set("Authorization", "Bearejr " + token)
+            .end(function (err, res) {
+                console.log(err)
+                expect(res.status).to.be.equal(200);
+                expect(res.headers['content-type']).to.equal('application/json; charset=utf-8')
                 expect(res.body.stat).to.be.equal('success')
                 done()
             });
