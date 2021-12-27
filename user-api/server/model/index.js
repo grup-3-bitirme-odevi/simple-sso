0b73e21a6f19ee75be33285f432b6dbbf3db0ea6
yrtby <anilcanyurtbay@hotmail.com>
2021-12-27 14:03:02 +0300

    Set sequelize logging -> false

--- user-api/server/model/index.js
+++ user-api/server/model/index.js
@@ -3,8 +3,7 @@ const { Sequelize, DataTypes } = require("sequelize");
 const sequelize = new Sequelize(config.DB, config.USER, config.PASSWORD, {
   host: config.HOST,
   dialect: config.dialect,
-  operatorsAliases: false,
-
+  logging: false,
   pool: {
     max: config.pool.max,
     min: config.pool.min,
