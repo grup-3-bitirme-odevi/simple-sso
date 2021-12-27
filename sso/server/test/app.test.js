0fc19898fd99f5f4e806442f10096ae9016698d1
Ömer Faruk ŞAHİN <79583574+omerfaruk-sahin@users.noreply.github.com>
2021-12-27 17:16:11 +0300

    Update app.test.js

--- sso/server/test/app.test.js
+++ sso/server/test/app.test.js
@@ -5,20 +5,20 @@ chai.use(chaiHttp);
 const server = require("../app")
 
 describe("SSO contoller", async () => {
-    before(function( done ) {
-        server.listen(3100, function(err) {
-            done(err);
-        })
-    });
+    // before(function( done ) {
+    //     server.listen(3100, function(err) {
+    //         done(err);
+    //     })
+    // });
     
-    afterAll(function( done ) {
-        server.close(function(err) {
-            done(err);
-        });
-    });
+    // afterAll(function( done ) {
+    //     server.close(function(err) {
+    //         done(err);
+    //     });
+    // });
     
     it("Post isAuthorized", done => {
-        chai.request(server).post("/?redirectURL=http://localhost:3020")
+        chai.request(server).post("?redirectURL=http://localhost:3020")
             .send({
                 username: "admin",
                 password: "123456",
@@ -29,12 +29,8 @@ describe("SSO contoller", async () => {
                 const token = res.body.access_token
                 global.token = token
 
-                // try {expect(res.status).to.be.equal(200)}
-                // catch(err) {
-                //     console.log('hata')
-                //     expect(res.status).to.be.equal(404)
-                // }
-
+            
+                expect(res.status).to.be.equal(200)
                 expect(res.headers['content-type']).to.equal('application/json; charset=utf-8')
                 expect(res.body.stat).to.be.equal('success')
                 expect(res.body.message).to.be.equal('login success')
@@ -44,10 +40,10 @@ describe("SSO contoller", async () => {
             });
     });
     it("Get isAccessTokenValid", done => {
-        chai.request(server).get("/avalidate")
-            .set("Authorization", "Bearejr " + token)
+        chai.request(server).get("/validate")
+            .set("Authorization", "Bearer " + token)
             .end(function (err, res) {
-                console.log(err)
+                console.log(res.body.stat)
                 expect(res.status).to.be.equal(200);
                 expect(res.headers['content-type']).to.equal('application/json; charset=utf-8')
                 expect(res.body.stat).to.be.equal('success')
