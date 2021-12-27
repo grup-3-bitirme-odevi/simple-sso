084538bfea51d28bfd153bd4379d37fe9476b9e1
yrtby <anilcanyurtbay@hotmail.com>
2021-12-27 13:46:56 +0300

    Revert "Revert "Apply morgan logger -> SSO AUTH & USER MANAGER""
    This reverts commit cc9bba56411c4d3cde0c603ffeeb146c98f4b3d2.

--- sso/server/app.js
+++ sso/server/app.js
@@ -1,7 +1,26 @@
-const express = require('express')
-const cors = require('cors');
+const db = require("./model");
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const Writable = require("stream").Writable;
+const Log = db.log;
+
+class StreamHook extends Writable {
+  write(line) {
+		let logModule = "SSO-AUTH"
+		let logIp = line.split("|")[0]
+		let logMethod = line.split("|")[1]
+		let logUrl = line.split("|")[2]
+		let logStatus = line.split("|")[3]
+		let logResLength = line.split("|")[4]
+		let logResTime = line.split("|")[5]
+
+		logger(logModule, logIp, logMethod, logUrl, logStatus, logResLength, logResTime);
+  }
+}
+
 // DB Connection
-require('./config/databaseConfig')
+require("./config/databaseConfig");
 
 // Route Path
 const ssoRoute = require("./route/ssoRoute");
@@ -10,9 +29,18 @@ const ssoRoute = require("./route/ssoRoute");
 const app = express();
 
 // Middlewares
-app.use(express.urlencoded({extended:true}))
-app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(cors());
+
+// Morgan Logger
+let streamHook = new StreamHook();
+app.use(
+  morgan(
+    ":remote-addr | :method | :url | :status | :res[content-length] | - :response-time ms",
+    { stream: streamHook }
+  )
+);
 
 // Routes
 app.use("/", ssoRoute);
@@ -20,5 +48,23 @@ app.use("/", ssoRoute);
 // App Start
 const port = 3100;
 app.listen(port, () => {
-    console.log('Server Started')
+  console.log("Server Started");
 });
+
+
+const logger = async (module, ip, method, url, status, res_length, res_ms) => {
+	try{
+		await Log.create({
+			module: module,
+			ip: ip,
+			method: method,
+			url: url,
+			status: status,
+			res_length: res_length,
+			res_ms: res_ms
+		});
+	} catch(error){
+		console.log(error);
+	}
+
+}
---
+++ sso/server/model/Log.js
@@ -0,0 +1,49 @@
+module.exports = (sequelize, DataTypes) => {
+  const Log = sequelize.define(
+    "log",
+    {
+      id: {
+        type: DataTypes.INTEGER(11),
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      module: {
+        type: DataTypes.STRING(45),
+        allowNull: false,
+      },
+      ip: {
+        type: DataTypes.STRING(45),
+        allowNull: false,
+      },
+      method: {
+        type: DataTypes.STRING(45),
+        allowNull: false,
+      },
+      url: {
+        type: DataTypes.STRING(45),
+        allowNull: false,
+      },
+      status: {
+        type: DataTypes.STRING(45),
+        allowNull: false,
+      },
+      res_length: {
+        type: DataTypes.STRING(45),
+        allowNull: false,
+      },
+      res_ms: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+    },
+    {
+      createdAt: true,
+      updatedAt: false,
+    }
+  );
+
+  return Log;
+};
+
+// Logger::  ::1 - - [27/Dec/2021:09:52:56 +0000] "GET /validate HTTP/1.1" 200 18 "-" "PostmanRuntime/7.28.4"
+// Module: sso-auth :  Date :
